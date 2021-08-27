cf39487589b5b1d1a2dde8f55ab4bd84ade78536
blelump <blelump@martex.nom.pl>
2021-08-27 21:53:41 +0200

    Add simple deserialization

--- src/controller.rs
+++ src/controller.rs
@@ -1,10 +1,15 @@
 use super::io::*;
+use crate::state::Bundle;
 use std::io::Read;
 
-pub fn load(source: &mut dyn Read) {
-    let mut buffer = String::new();
-    source.read_to_string(&mut buffer);
-    println!("{}", buffer);
+pub type GenericError = Box<dyn std::error::Error + Sync + Send>;
+pub type GenericResult<T> = Result<T, GenericError>;
+
+pub fn load(source: &mut dyn Read) -> GenericResult<()> {
+    let v: Bundle = serde_json::from_reader(source)?;
+    println!("{:?}", v);
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -14,8 +19,11 @@ mod tests {
     #[test]
     fn loads_json_from_str() {
         let data = r#"
- {"schema_base":{"@context":"https://odca.tech/v1","name":"Presentation Status","type":"spec/schema_base/1.0","description":"","classification":"","issued_by":"","attributes":{"presentation_urn":"Text","verified":"Boolean"},"pii_attributes":[]},"overlays":[{"@context":"https://odca.tech/overlays/v1","type":"spec/overlay/label/1.0","issued_by":"","role":"","purpose":"","schema_base":"hl:4YjW5R27kqiCDX5Tq6d3kNXmbK7g9skt6jjYW5iVZbL1","language":"en_US","attr_labels":{"presentation_urn":"Presentation URN","verified":"Verified"},"attr_categories":["_cat-1_"],"cat_labels":{"_cat-1_":""},"cat_attributes":{"_cat-1_":["presentation_urn","verified"]}},{"@context":"https://odca.tech/overlays/v1","type":"spec/overlay/character_encoding/1.0","issued_by":"","role":"","purpose":"","schema_base":"hl:4YjW5R27kqiCDX5Tq6d3kNXmbK7g9skt6jjYW5iVZbL1","default_character_encoding":"utf-8","attr_character_encoding":{"presentation_urn":"utf-8","verified":"utf-8"}}]}
+ { 
+ "capture_base": {"type": "abc", "classification": "def", "attributes": {}, "pii": []},
+ "overlays": []
+ }
         "#;
-        load(&mut data.as_bytes());
+        load(&mut data.as_bytes()).unwrap();
     }
 }
--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,13 +1,26 @@
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-pub enum CaptureBase {
-    Type(String),
-    Classification(String),
-    Attributes(HashMap<String, AttributeType>),
-    Pii(Vec<String>),
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Bundle {
+    pub capture_base: CaptureBase,
+    pub overlays: Vec<Overlays>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CaptureBase {
+    #[serde(alias = "type")]
+    schema_type: String,
+    classification: String,
+    attributes: HashMap<String, AttributeType>,
+    pii: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub enum AttributeType {
     Text,
     Number,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Overlays {}
