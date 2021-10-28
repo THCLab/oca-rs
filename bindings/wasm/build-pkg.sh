a6bbafbaaef398686219752a8278766e6919e21f
Marcin Olichwiruk <marcin@opensoftware.pl>
2021-10-29 01:19:54 +0200

    Handle building cross-env bindings/wasm package

---
+++ bindings/wasm/build-pkg.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+OUTPUT_DIR=$1
+if [ -z "$OUTPUT_DIR" ]
+then
+  OUTPUT_DIR="pkg"
+fi
+
+cargo build --target wasm32-unknown-unknown --release
+wasm-bindgen target/wasm32-unknown-unknown/release/oca_js.wasm --target nodejs --out-dir ./$OUTPUT_DIR/nodejs
+wasm-bindgen target/wasm32-unknown-unknown/release/oca_js.wasm --target web --out-dir ./$OUTPUT_DIR/web
+
+PACKAGE_VERSION=$(cat ./Cargo.toml \
+  | grep version \
+  | head -1 \
+  | awk -F= '{ print $2 }' \
+  | sed 's/"//g' \
+  | tr -d '[[:space:]]')
+
+
+cp ./pkg_templates/package.json ./$OUTPUT_DIR
+sed -i "s/_VERSION_/$PACKAGE_VERSION/g" ./$OUTPUT_DIR/package.json
+cp ./pkg_templates/README.md ./$OUTPUT_DIR
---
+++ bindings/wasm/pkg_templates/README.md
@@ -0,0 +1,12 @@
+# [![WASM Build Status]][WASM actions] [![NPM version]][npmjs.com]
+
+[WASM Build Status]: https://github.com/THCLab/oca-rust/actions/workflows/wasm.yml/badge.svg?branch=main
+[WASM actions]: https://github.com/THCLab/oca-rust/actions/workflows/wasm.yml
+[NPM version]: https://img.shields.io/npm/v/oca.js
+[npmjs.com]: https://www.npmjs.com/package/oca.js
+[oca-rust repo]: https://github.com/THCLab/oca-rust
+
+## oca.js
+
+_generated from [oca-rust][oca-rust repo]_
+
