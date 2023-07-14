b2457a9ba3bc715102a4c0eb6b7bc078c01e75a2
Marcin Olichwiruk <21108638+olichwiruk@users.noreply.github.com>
2023-07-14 20:06:30 +0200

    feat(oca-rs): add error handling for get_oca_bundle_steps function

--- oca/src/facade/fetch.rs
+++ oca/src/facade/fetch.rs
@@ -16,24 +16,26 @@ impl Facade {
     pub fn get_oca_bundle_steps(&self, said: String) -> Result<Vec<OCABuildStep>, Vec<String>> {
         let mut said = said;
         #[allow(clippy::borrowed_box)]
-        fn extract_operation(db: &Box<dyn DataStorage>, said: &String) -> (String, oca_ast::ast::Command) {
-            let r = db.get(&format!("oca.{}.operation", said)).unwrap().unwrap();
+        fn extract_operation(db: &Box<dyn DataStorage>, said: &String) -> Result<(String, oca_ast::ast::Command), Vec<String>> {
+            let r = db.get(&format!("oca.{}.operation", said))
+                .map_err(|e| vec![format!("{}", e)])?
+                .ok_or_else(|| vec![format!("No history found for said: {}", said)])?;
 
             let said_length = r.first().unwrap();
             let parent_said = String::from_utf8_lossy(&r[1..*said_length as usize + 1]).to_string();
             let op_length = r[*said_length as usize + 1];
             let op = String::from_utf8_lossy(&r[*said_length as usize + 2..*said_length as usize + 2 + op_length as usize]).to_string();
 
-            (
+            Ok((
                 parent_said,
                 serde_json::from_str(&op).unwrap()
-            )
+            ))
         }
 
         let mut history: Vec<OCABuildStep> = vec![];
 
         loop {
-            let (parent_said, command) = extract_operation(&self.db, &said);
+            let (parent_said, command) = extract_operation(&self.db, &said)?;
             history.push(
                 OCABuildStep {
                     parent_said: parent_said.clone().parse().ok(),
