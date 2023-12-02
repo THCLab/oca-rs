dc92abe17ec9527fdbf4a5f51e89b4ca37048099
Robert Mitwicki <robert@thclab.online>
2023-12-02 01:03:42 +0100

    fix(oca-ast): fix oca box  test

--- oca-ast/src/ast.rs
+++ oca-ast/src/ast.rs
@@ -86,7 +86,7 @@ impl FromStr for AttributeType {
             "Array[Reference]" => Ok(AttributeType::ArrayReference),
             _ => {
                 if let Some((attr_type, _)) = s.split_once(':') {
-                    if attr_type == "Reference" {
+                    if attr_type == "refs" {
                         Ok(AttributeType::Reference)
                     } else if attr_type == "Array[Reference" {
                         Ok(AttributeType::ArrayReference)
